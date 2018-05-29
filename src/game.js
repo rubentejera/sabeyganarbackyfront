9dedda5d1955c87071fc5e5d4865e4cd37fab933
rubentejera <ruben.tejera@leanmind.es>
2018-05-29 16:30:48 +0100

    Green:should not have any sectedquestion when go to next questions , Refactor renderQuestion() and bodymain.html

--- src/game.js
+++ src/game.js
@@ -9,6 +9,8 @@ export default function createGame(createQuestionsNavigator, client) {
     let timerId;
     let countdown;
     let theQuestionNavigator;
+    let answerList;
+    let answerOption;
 
     function start(){
         questionsContainer = document.querySelector('.questions__container');
@@ -19,6 +21,7 @@ export default function createGame(createQuestionsNavigator, client) {
         questionAnswers = document.querySelectorAll('.question--answer');
         radioAnswersList = document.querySelectorAll('.input-radio');
         nextQuestionButton = document.getElementById('next--question--button');
+        answerList = document.getElementById('answer--list');
 
 
         nextQuestionButton.addEventListener('click', onNextQuestion);
@@ -84,10 +87,60 @@ export default function createGame(createQuestionsNavigator, client) {
         showComponent(questionsContainer);
         questionTitle.innerHTML = (question.title);
         questionTitle.setAttribute('id', question.id);
-        for (let i = 0; i < question.answers.length; i++) {
-            questionAnswers[i].innerHTML = (question.answers[i].answer);
-            radioAnswersList[i].setAttribute('id', question.answers[i].id);
+
+        // for (let i = 0; i < question.answers.length; i++) {
+        //     questionAnswers[i].innerHTML = (question.answers[i].answer);
+        //     radioAnswersList[i].setAttribute('id', question.answers[i].id);
+        // }
+        answerOption = document.querySelectorAll('.answer--option');
+        console.log("***********************",answerOption.length);
+
+        if(answerOption.length>0){
+            answerOption.forEach(function(element){
+                console.log("BORRO -->", element);
+                element.remove();
+
+            });
         }
+
+        for (let i = 0; i < question.answers.length; i++) {
+            var li = document.createElement("li",/*{class:"answer--option"}*/);
+            li.setAttribute("class","answer--option");
+
+            var input = document.createElement("input"/*, {type:"radio",value:"",name:"radAnswer",class:"input-radio",id:question.answers[i].id}*/);
+            input.setAttribute("type", "radio");
+            input.setAttribute("value", "");
+            input.setAttribute("name", "radAnswer");
+            input.setAttribute("class", "input-radio");
+            input.setAttribute("id", question.answers[i].id);
+
+
+            var label = document.createElement("label"/*,{class:"question--answer"}*/);
+            label.setAttribute("class","question--answer");
+
+            var answerText = document.createTextNode(question.answers[i].answer);
+
+            label.appendChild(answerText);
+            li.appendChild(input);
+            li.appendChild(label);
+            answerList.appendChild(li);
+        }
+
+        // var li = document.createElement("li",{class:"answer--option"});
+        // var input = document.createElement("input", {type:"radio",value:"",name:"radAnswer",class:"input-radio"});
+        // var label = document.createElement("label",{class:"question--answer"});
+
+
+        // li.appendChild(input);
+        // li.appendChild(label);
+        // console.log(li);
+
+
+    // <li class="answer--option">
+    //         <input type="radio" value="" name="radAnswer" class="input-radio">
+    //         <label class="question--answer"></label>
+    //  </li>
+
     }
 
     function showComponent(component){
