c17b8f7b86e19389435e0296220c3ed20d2142de
rubentejera <ruben.tejera@leanmind.es>
2018-05-30 17:42:00 +0100

    Extract function timer() to new file timer.js

--- src/game.js
+++ src/game.js
@@ -1,3 +1,4 @@
+import timer from './timer.js';
 export default function createGame(createQuestionsNavigator, client) {
 
     let startButtonUI;
@@ -92,51 +93,51 @@ export default function createGame(createQuestionsNavigator, client) {
         }
     }
 
-    function timer(seconds, event){
-        let saveSeconds = seconds;
-        let counter = 0;
-        let interval;
-
-        function start() {
-            interval = setInterval(function () {
-                intervalAction();
-            }, 1000);
-        }
-
-        function intervalAction(){
-            decrease();
-            event(counter);
-        }
-
-        function stop() {
-            clearInterval(interval);
-        }
-
-        function reset(){
-            counter = saveSeconds;
-        }
-
-        function decrease(){
-            counter--;
-        }
-
-        function get(){
-            return counter;
-        }
-
-        function restart(){
-            stop();
-            reset();
-            start();
-            event(counter);
-        }
-
-        return{
-            restart,
-            get,
-            stop,
-        }
-    }
+    // function timer(seconds, event){
+    //     let saveSeconds = seconds;
+    //     let counter = 0;
+    //     let interval;
+    //
+    //     function start() {
+    //         interval = setInterval(function () {
+    //             intervalAction();
+    //         }, 1000);
+    //     }
+    //
+    //     function intervalAction(){
+    //         decrease();
+    //         event(counter);
+    //     }
+    //
+    //     function stop() {
+    //         clearInterval(interval);
+    //     }
+    //
+    //     function reset(){
+    //         counter = saveSeconds;
+    //     }
+    //
+    //     function decrease(){
+    //         counter--;
+    //     }
+    //
+    //     function get(){
+    //         return counter;
+    //     }
+    //
+    //     function restart(){
+    //         stop();
+    //         reset();
+    //         start();
+    //         event(counter);
+    //     }
+    //
+    //     return{
+    //         restart,
+    //         get,
+    //         stop,
+    //     }
+    // }
 
     function handlerEventTime(time){
         updateTimerUI(time);
---
+++ src/timer.js
@@ -0,0 +1,45 @@
+export default function timer(seconds, event){
+    let saveSeconds = seconds;
+    let counter = 0;
+    let interval;
+
+    function start() {
+        interval = setInterval(function () {
+            intervalAction();
+        }, 1000);
+    }
+
+    function intervalAction(){
+        decrease();
+        event(counter);
+    }
+
+    function stop() {
+        clearInterval(interval);
+    }
+
+    function reset(){
+        counter = saveSeconds;
+    }
+
+    function decrease(){
+        counter--;
+    }
+
+    function get(){
+        return counter;
+    }
+
+    function restart(){
+        stop();
+        reset();
+        start();
+        event(counter);
+    }
+
+    return{
+        restart,
+        get,
+        stop,
+    }
+}
