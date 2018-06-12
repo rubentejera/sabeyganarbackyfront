3e45fde570c638f8af68bbed773f774021b83d46
rubentejera <ruben.tejera@leanmind.es>
2018-06-12 15:44:34 +0100

    Change questions to testQuestion on spec.js

--- __tests__/spec.js
+++ __tests__/spec.js
@@ -1,106 +1,15 @@
 import createGame from '../src/game';
 import createQuestionsNavigator from '../src/questionsNavigator';
-import score from '../src/score';
 import gameRules from '../src/gameRules';
+import testQuestions from '../src/testQuestions';
 
 jest.setTimeout(30000);
 
 const pug = require('pug');
 
-let questions = [
-    {
-        id: 10,
-        title: 'Foo',
-        answers: [
-            {id: 0, answer: '25'},
-            {id: 1, answer: '33'},
-            {id: 2, answer: '37'}
-        ],
-        correctAnswer: {id: 2}
-    },
-    {
-        id: 11,
-        title: 'Pero que dices muchacho?',
-        answers: [
-            {id: 0, answer: 'Lusaka'},
-            {id: 1, answer: 'Harare'},
-            {id: 2, answer: 'Canarias'}
-        ],
-        correctAnswer: {id: 2}
-    },
-    {
-        id: 12,
-        title: 'Nombre de Colón?',
-        answers: [
-            {id: 0, answer: 'Cristobal'},
-            {id: 1, answer: 'Santiago'},
-            {id: 2, answer: 'Pepito'}
-        ],
-        correctAnswer: {id: 0}
-    }
-];
-
-// describe("the testQuestions navigator", () => {
-//     let questionsNavigator;
-//     let totalQuestions;
-//     let currentQuestion;
-//
-//     beforeEach(function () {
-//         questionsNavigator = createQuestionsNavigator(questions);
-//         totalQuestions = questions.length;
-//     });
-//
-//     it("get the current question", () => {
-//         currentQuestion = questionsNavigator.getCurrentQuestion();
-//         expect(questions).toContain(currentQuestion);
-//     });
-//
-//     it("is always pointing to a question", () => {
-//         let questionAfterTheLast;
-//
-//         for (let i = 0; i < totalQuestions; i++) {
-//             if (i == 0) {
-//                 questionsNavigator.getCurrentQuestion();
-//             } else {
-//                 questionsNavigator.getNextQuestion();
-//             }
-//         }
-//
-//         questionAfterTheLast = questionsNavigator.getNextQuestion();
-//
-//         expect(questions).toContain(questionAfterTheLast);
-//         expect(questionsNavigator.areThereNonVisitedQuestions()).toBeFalsy();
-//
-//
-//     });
-//
-//     it("does not repeat the last question", () => {
-//         let lastQuestionVisited = {};
-//
-//         for (let i = 0; i < totalQuestions; i++) {
-//             if (i == 0) {
-//                 currentQuestion = questionsNavigator.getCurrentQuestion();
-//             } else {
-//                 currentQuestion = questionsNavigator.getNextQuestion();
-//             }
-//             expect(currentQuestion).not.toEqual(lastQuestionVisited);
-//             lastQuestionVisited = currentQuestion;
-//         }
-//     });
-//
-//     it("knows when the testQuestions are all visited", () => {
-//         for (let i = 0; i < totalQuestions; i++) {
-//             if (i == 0) {
-//                 questionsNavigator.getCurrentQuestion();
-//             } else {
-//                 questionsNavigator.getNextQuestion();
-//             }
-//         }
-//         expect(questionsNavigator.areThereNonVisitedQuestions()).toBeFalsy();
-//     });
-// });
 
 describe("the game", function () {
+    let questions = testQuestions().getQuestions();
     let game;
     let theQuestionNavigator;
 
@@ -279,47 +188,3 @@ describe("the game", function () {
         expect(questionTitle.innerHTML).toEqual(questions[1].title);
     }
 });
-
-// describe("Scoreboard", function () {
-//     let scoreboardGame;
-//
-//     beforeEach(function () {
-//         scoreboardGame = new score();
-//     });
-//
-//     it("should return 0 when score is started", function () {
-//         expect(scoreboardGame.getScore()).toEqual(0);
-//     });
-//
-//     it("should increment a quantity to the score", function () {
-//         scoreboardGame.increment(3);
-//         expect(scoreboardGame.getScore()).toEqual(3);
-//     });
-//
-//     it("should decrement a quantity to the score", function () {
-//         scoreboardGame.increment(3);
-//         scoreboardGame.decrement(2);
-//         expect(scoreboardGame.getScore()).toEqual(1);
-//     });
-//
-//     it("should be 0 the minimum score", function () {
-//         scoreboardGame.decrement(2);
-//         expect(scoreboardGame.getScore()).toEqual(0);
-//     });
-//
-// });
-
-// describe("Statistics", function () {
-//
-//
-//     // beforeEach(function () {
-//     //     scoreboardGame = new score();
-//     // });
-//
-//     it("shouldn't be show the statistics if the game is started", function () {
-//         getS
-//         expect(scoreboardGame.getScore()).toEqual(0);
-//     });
-//
-//
-// });
