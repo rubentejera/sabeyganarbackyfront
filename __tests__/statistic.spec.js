1d86e0072fbcd75a7f187f48ffeccef4eb1d3063
rubentejera <ruben.tejera@leanmind.es>
2018-06-22 12:11:06 +0100

    Red:statistic:should return number of success answers

--- __tests__/statistic.spec.js
+++ __tests__/statistic.spec.js
@@ -1,4 +1,5 @@
 import statistic from "../src/statistic";
+import gameRules from "../src/gameRules";
 
 describe("Statistic", function () {
     let statisticTest;
@@ -19,15 +20,68 @@ describe("Statistic", function () {
         expect(statisticTest.getAllData()).toEqual([]);
     });
 
+    it("should return number of success answers", function () {
+        expect(statisticTest.getSuccessAnswers()).toEqual(3);
+    });
+
+
     function addRandomDataToStatistic() {
         statisticTest.addData({
             idQuestion:1,
+            idCorrectAnswer:1,
             isAnswered:true,
             idAnswerSelected:1,
-            idCorrectAnswer:1,
             isCorrect:true,
-            score: 3,
-            elapsedSeconds:2,
+            score: gameRules().pointsToAddQuickReplySuccess,
+            elapsedSeconds:gameRules().maxTimeQuickReply,
+        });
+
+        statisticTest.addData({
+            idQuestion:2,
+            idCorrectAnswer:3,
+            isAnswered:true,
+            idAnswerSelected:3,
+            isCorrect:true,
+            score: gameRules().pointsToAddNormalReplySuccess,
+            elapsedSeconds:gameRules().maxTimeNormalReply,
+        });
+
+        statisticTest.addData({
+            idQuestion:3,
+            idCorrectAnswer:3,
+            isAnswered:true,
+            idAnswerSelected:3,
+            isCorrect:true,
+            score: gameRules().pointsToAddSlowReplySuccess,
+            elapsedSeconds:gameRules().secondsPerQuestion,
+        });
+
+        statisticTest.addData({
+            idQuestion:4,
+            idCorrectAnswer:3,
+            isAnswered:true,
+            idAnswerSelected:2,
+            isCorrect:false,
+            score: gameRules().pointsToSubtractQuickReplyFail,
+            elapsedSeconds:gameRules().maxTimeQuickReply,
+        });
+
+        statisticTest.addData({
+            idQuestion:5,
+            idCorrectAnswer:3,
+            isAnswered:true,
+            idAnswerSelected:2,
+            isCorrect:false,
+            score: gameRules().pointsToSubtractNormalReplyFail,
+            elapsedSeconds:gameRules().maxTimeNormalReply,
+        });
+
+        statisticTest.addData({
+            idQuestion:6,
+            idCorrectAnswer:2,
+            isAnswered:false,
+            score: gameRules().pointsToSubtractNoReply,
+            elapsedSeconds:gameRules().secondsPerQuestion,
         });
     }
 
